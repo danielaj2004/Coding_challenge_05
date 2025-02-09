f31b9c3eb68e33059badd6f5dc68648e79d65dd0
danielaj2004 <danielajoya12@gmail.com>
2025-02-08 21:11:55 -0500

    Task 2 - Order Details.

--- cc_5.js
+++ cc_5.js
@@ -9,3 +9,15 @@ const customerInfo = {
 console.log(`Name: ${customerInfo.name}`); // logging customer name
 console.log(`Age: ${customerInfo.age}`); // logging customer age
 console.log(`Email: ${customerInfo.email}`); // logging customer email
+
+// Task 1: object methods
+
+const order = {
+    orderID: 12345,
+    totalAmount: 150,
+    status: "processing",
+    displayOrder() {
+        console.log(`Order ID: ${this.orderID}, Total: $${this.totalAmount}, Status: ${this.status}`);
+    }
+}; // console log of customer order details
+order.displayOrder();
