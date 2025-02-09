25f59bf731d5e5d889bd4c3bfb365a4dcacaf04a
danielaj2004 <danielajoya12@gmail.com>
2025-02-08 21:02:44 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,11 @@
+// Task 1: Object Properties
+
+const customerInfo = {
+    name: "John doe",
+    age: 35,
+    email: "john.doe@gmail.com"
+} // object as customer
+
+console.log(`Name: ${customerInfo.name}`); // logging customer name
+console.log(`Age: ${customerInfo.age}`); // logging customer age
+console.log(`Email: ${customerInfo.email}`); // logging customer email
